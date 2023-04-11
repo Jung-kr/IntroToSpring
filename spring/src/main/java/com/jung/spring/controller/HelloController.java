96bc4e1d492464479b4abe20717f9f6e4df946d0
jung-kr <jungwy98@gmail.com>
2023-04-11 14:56:34 +0900

    feat : MVC패턴을 이용한 hello페이지 구현

--- spring/src/main/java/com/jung/spring/controller/HelloController.java
+++ spring/src/main/java/com/jung/spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.jung.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,4 +13,12 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    // MVC 패턴과 템플릿 엔진(Thymeleaf) 이용하여 hello-template.html 반환
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
 }
