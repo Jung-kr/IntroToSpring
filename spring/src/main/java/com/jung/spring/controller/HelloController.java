76a3de704be8d94d1e891485fefefe8acb1e1441
jung-kr <jungwy98@gmail.com>
2023-04-14 02:31:15 +0900

    feat : 회원 도메인 생성

--- spring/src/main/java/com/jung/spring/controller/HelloController.java
+++ spring/src/main/java/com/jung/spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -20,5 +21,35 @@ public class HelloController {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    // API 방식으로 문자 보내기
+    @GetMapping("hello-string")
+    @ResponseBody   // http 응답시 body 부에 데이터를 직접 넣어주겠다는 어노테이션
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    // API 방식으로 데이터 보내기 (객체 리턴하면 json 반환이 default)
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloAPI(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    // 데이터를 저장하기 위한 객체 생성
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 
 }
