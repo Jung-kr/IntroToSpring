c3298398a8b0853ce5e08b262a373107fb93fda0
jung-kr <jungwy98@gmail.com>
2023-04-15 16:57:52 +0900

    feat : 회원 등록 컨트롤러 및 폼 구현

--- spring/src/main/java/com/jung/spring/controller/MemberController.java
+++ spring/src/main/java/com/jung/spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.jung.spring.controller;
 
+import com.jung.spring.domain.Member;
 import com.jung.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,21 @@ public class MemberController {
     }
      */
 
+    // 회원가입 페이지
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    // 회원가입 페이지에서 이름을 입력했을 때
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 
 }
