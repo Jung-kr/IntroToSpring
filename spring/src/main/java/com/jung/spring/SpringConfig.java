3c4a0131c96155a5134153bec36a1da60e34bcac
jung-kr <jungwy98@gmail.com>
2023-04-15 22:52:00 +0900

    feat : 순수 Jdbc 이용한 데이터 엑세스

--- spring/src/main/java/com/jung/spring/SpringConfig.java
+++ spring/src/main/java/com/jung/spring/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.jung.spring;
 
+import com.jung.spring.repository.JdbcMemberRepository;
 import com.jung.spring.repository.MemberRepository;
 import com.jung.spring.repository.MemoryMemberRepository;
 import com.jung.spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);    // 순수 Jdbc를 이용한 데이터 엑세스
     }
 }
