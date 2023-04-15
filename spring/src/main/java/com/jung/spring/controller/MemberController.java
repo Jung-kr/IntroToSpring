8f8d6dfcf50bd6253f8b24511992f8c360d859a6
jung-kr <jungwy98@gmail.com>
2023-04-15 16:20:23 +0900

    feat : SpringConfig 파일에 직접 스프링 빈 등록

--- spring/src/main/java/com/jung/spring/controller/MemberController.java
+++ spring/src/main/java/com/jung/spring/controller/MemberController.java
@@ -2,13 +2,33 @@ package com.jung.spring.controller;
 
 import com.jung.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class MemberController {
 
+    // 1)생성자를 톻한 의존성 주입
     private final MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    // 2)필드롤 통한 의존성 주입
+    /*
+    @Autowired private MemberService memberService;
+     */
+
+    // 3)setter를 통한 의존성 주입
+    /*
+    private MemberService memberService;
+
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+     */
+
+
 }
